1fe176e195baf8a17d72a0bd2ea9b09e01e6808f
Mikeyas <mikeyas.lira@gmail.com>
2025-07-06 23:56:30 -0300

    primeiro projeto

---
+++ fastapi_zero/app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def reed_root():
+    return {'message': 'Olá Mundo!'}
---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.json() == {'message': 'Olá Mundo!'}
