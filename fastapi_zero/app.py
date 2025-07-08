6cdf8285c9644d7290d9b670a9c790b4945422cd
Mikeyas <mikeyas.lira@gmail.com>
2025-07-07 22:18:28 -0300

    novo teste e novo endpoint

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import MainSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=MainSchema)
 def reed_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def read_html():
+    return """
+    <html>
+        <head>
+            <title>FastAPI Zero</title>
+        </head>
+        <body>
+            <h1>Olá Mundo!</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
@@ -9,3 +11,12 @@ def test_root_deve_retornar_ola_mundo():
     response = client.get('/')
 
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_html_deve_retornar_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Olá Mundo!</h1>' in response.text
